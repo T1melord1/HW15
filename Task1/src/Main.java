318c50efc76f6c69c40e22319d34f266a2d59959
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-31 14:48:05 +0300

    Напишите код который распечатает букву и ее код для всех букв русского алфавита, нужно распечатать коды букв в двух регистрах

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -5,5 +5,14 @@ public class Main {
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
     public static void main(String[] args) {
         //todo код писать тут
-    }
+        String ABC = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        String a = String.valueOf(ABC);
+        for (int i = 1; i <= ABC.length(); i++) {
+            for (int j = 0; j < ABC.length(); j++) {
+                int perevod = ((int) a.charAt(j));
+                System.out.print("буква алфавита №" + i++ + " " + a.charAt(j) + " - ");
+                System.out.println(perevod);
+            }
+        }
+        }
 }
