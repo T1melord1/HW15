26e9440efbea994604c25f8f6f8a5455b9fe3ef5
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-31 18:22:58 +0300

    Отброс имени из ввода

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+
 public class Main {
+
     public static void main(String[] args) {
         //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        razdel(credentials());
+
+    }
+
+    public static String credentials() {
+        System.out.println("Введите ФИО через пробел:");
+        String input = new Scanner(System.in).nextLine();
+        return input;
+
+    }
+    public static void razdel(String input){
+        System.out.println(input.substring(input.indexOf(" ")));
     }
 }
