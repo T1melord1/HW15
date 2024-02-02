418778e572a35e05af0b43aa6486c28f3f5a0727
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-02 10:06:25 +0300

    Всегда Ввод не верен

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,22 +1,47 @@
 import java.util.Scanner;
-import java.util.SortedMap;
 
 public class Main {
+    public static final String ABC = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя-";
 
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
-        razdel(credentials());
 
+        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        String credentials = credentials();
+        if(!credentials.isEmpty()) {
+            razdel(credentials);
+        }
     }
 
     public static String credentials() {
         System.out.println("Введите ФИО через пробел:");
         String input = new Scanner(System.in).nextLine();
-        return input;
-
+        boolean isValid = true;
+        for (int i = 0; i < input.length(); i++) {
+            if (!ABC.contains(String.valueOf(input.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid){
+            return input;
+        }
+        else {
+            System.out.println("Ввод не верен");
+            return "";
+        }
     }
-    public static void razdel(String input){
-        System.out.println(input.substring(input.indexOf(" "), input.lastIndexOf(" ")).trim());
-        System.out.println(input.substring(input.lastIndexOf(" ")).trim());
+
+    public static void razdel(String input) {
+        int firstSpace = input.indexOf(" ");
+        int secondSpace = input.indexOf(" ", firstSpace + 1);
+        if(firstSpace != -1 && secondSpace != -1) {
+            System.out.println("Фамилия: " + input.substring(0, input.indexOf(" " )).trim());
+            System.out.println("Имя: " + input.substring(input.indexOf(" "), input.lastIndexOf(" ")).trim());
+            System.out.println("Отчество: " + input.substring(input.lastIndexOf(" ")).trim());
+        }
+        else {
+            System.out.println("Ввод не верен");
+        }
+
     }
 }
